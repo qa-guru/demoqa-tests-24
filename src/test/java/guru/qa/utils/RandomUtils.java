0505babbcbf55cfaddab6fd73353076b64910382
svasenkov <aanher@gmail.com>
2024-01-12 19:50:35 +0100

    added random utils

---
+++ src/test/java/guru/qa/utils/RandomUtils.java
@@ -0,0 +1,61 @@
+package guru.qa.utils;
+
+import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtils {
+
+    public static void main(String[] args) {
+        System.out.println(getRandomString(10));
+        System.out.println(getRandomEmail());
+        System.out.println(getRandomAddress());
+        System.out.println(getRandomInt(111, 99999999));
+        System.out.println(getRandomPhone());
+        System.out.println(getRandomGender());
+    }
+
+    public static String getRandomString(int len) {
+//        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < len; i++)
+            result.append(AB.charAt(rnd.nextInt(AB.length())));
+
+        return result.toString();
+    }
+
+    public static String getRandomEmail() {
+        return getRandomString(10) + "@qa.guru";
+    }
+
+    public static String getRandomAddress() {
+        return getRandomString(10) + " " + getRandomString(10) + " " + getRandomString(10);
+    }
+
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    // +3 (263) 253 - 66 - 12
+    public static String getRandomPhone() {
+//        String a = "a123";
+//        String b = "b456";
+//        String c1 = a + " " + b; // "a123 b456"
+//        String c2 = String.format("%s %s", a, b); // "a123 b456"
+
+        return String.format("+%s (%s) %s - %s - %s", getRandomInt(1, 9), getRandomInt(111, 999),
+                getRandomInt(111, 999), getRandomInt(11, 99), getRandomInt(11, 99));
+    }
+
+    public static String getRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+
+        int index = getRandomInt(0, genders.length - 1);
+
+        return genders[index];
+    }
+
+}
