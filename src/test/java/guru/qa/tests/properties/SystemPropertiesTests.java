d72a9d84e81ff0c23c8c3b6169287bef73f10152
svasenkov <aanher@gmail.com>
2024-02-09 20:43:53 +0300

    added more System Properties tests

--- src/test/java/guru/qa/tests/properties/SystemPropertiesTests.java
+++ src/test/java/guru/qa/tests/properties/SystemPropertiesTests.java
@@ -1,5 +1,6 @@
 package guru.qa.tests.properties;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -37,8 +38,25 @@ public class SystemPropertiesTests {
 
     @Test
     @Tag("property1")
-    void systemProperty5Test() {
+    void systemProperty5Test() { // gradle property1_test -Dbrowser=chrome
         System.out.println(System.getProperty("browser", "safari"));
+    }
+
+    @Test
+    @Tag("property2")
+    void systemProperty6Test() {
+        String browser = System.getProperty("browser", "chrome");
+        String browserVersion = System.getProperty("browser_version", "100.0");
+        String browserSize = System.getProperty("browser_size", "1920x1080");
+
+        String browserConfig = String.format("Browser: %s, version: %s, window size: %s",
+                browser, browserVersion, browserSize);
+
+//        Bad practice
+//        String browserConfig = "Browser: " + browser + ", version: " + browserVersion + ", window size: " + browserSize;
+
+        // gradle property2_test -Dbrowser=mozilla -Dbrowser_version=96.0 -Dbrowser_size=1080x720
+        System.out.println(browserConfig);
     }
 
 }
